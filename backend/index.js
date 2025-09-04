1787c8c5956691b26e4207cb40b60d0878ab46e3
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-04 01:09:01 -0400

    added another reference to PORT

--- backend/index.js
+++ backend/index.js
@@ -7,5 +7,5 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 5001");
+  console.log("Server is running on port" + PORT);
 });
