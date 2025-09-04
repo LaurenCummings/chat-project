9d965c92c7e92d6c04289e74116d1561f932737b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-04 01:08:27 -0400

    fixed PORT constant

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 
-const app = express();
-
 dotenv.config();
 
-app.listen(5001, () => {
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
   console.log("Server is running on port 5001");
 });
