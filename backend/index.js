29f333badf2cd4e21ae85525ad4acc6e76a5492b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-04 01:03:28 -0400

    configured dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
+
 const express = require("express");
 
 const app = express();
+
+dotenv.config();
 
 app.listen(5001, () => {
   console.log("Server is running on port 5001");
