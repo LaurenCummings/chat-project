872c69d2b8e661992405158e852c4581f83773fe
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-06 03:15:18 -0400

    created getMessage function

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -13,3 +13,5 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 };
+
+export const getMessages = async (req, res) => {};
