ea6e8eb0925e48cc542eb329158c5d9763dda0a2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-12 03:46:36 -0400

    added signup page struture and styling

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore";
+import { MessageSquare } from "lucide-react";
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -16,7 +17,21 @@ const SignUpPage = () => {
     e.preventDefault();
   };
 
-  return <div>SignUpPage</div>;
+  return (
+    <div className="w-full max-w-md space-y-8">
+      <div className="text-center mb-8">
+        <div className="flex flex-col items-center gap-2 group">
+          <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+            <MessageSquare className="size-6 text-primary" />
+          </div>
+          <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+          <p className="text-base-content/60">
+            Get started with your free account
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
