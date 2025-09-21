9d3fd51a6c7a363ae6f2db4bf85c805aa1bea2d2
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-21 00:20:59 -0400

    added input to sign up page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -53,6 +53,24 @@ const SignUpPage = () => {
                 />
               </div>
             </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-medium">Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none"></div>
+                <input
+                  type="email"
+                  className={`input input-bordered w-full pl-10`}
+                  placeholder="you@example.com"
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, email: e.target.value })
+                  }
+                />
+              </div>
+            </div>
           </form>
         </div>
       </div>
