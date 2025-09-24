dea854e33f5f9efafd5aade589828b41abbac748
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-23 23:51:10 -0400

    added styling to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,13 @@ import { useAuthStore } from "../store/useAuthStore";
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
 
-  return <div>Navbar</div>;
+  return (
+    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blue-lg bg-base-100/80">
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full"></div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
