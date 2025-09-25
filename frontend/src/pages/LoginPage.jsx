df0fcf562960c3e0fbe641ae26c684be9d8ea608
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-24 23:15:54 -0400

    added state constants

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
   return <div>LoginPage</div>;
 };
 
