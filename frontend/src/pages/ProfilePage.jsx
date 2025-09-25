b07e84ce0190e76856fcb052f14a7f3665f5b520
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-24 23:51:14 -0400

    started adding styling for profile page

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,7 +1,17 @@
 import { useAuthStore } from "../store/useAuthStore";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const { authUser, isUpdatingProfile, updateProfile } = useAuthStore();
+
+  const handleImageUpload = async (e) => {};
+
+  return (
+    <div className="h-screen pt-20">
+      <div className="max-w-2xl mx-auto p-4 py-8">
+        <div className="bg-base-300 rounded-xl p-6 space-y-8"></div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePage;
