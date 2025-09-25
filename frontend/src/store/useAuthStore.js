e3da7dcf1242c33e0ae27d483d9f97b68c4a2ba0
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-24 23:36:22 -0400

    added toast for successful login

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -40,6 +40,7 @@ export const useAuthStore = create((set) => ({
     try {
       const res = await axiosInstance.post("auth/login", data);
       set({ authUser: res.data });
+      toast.success("Logged in successfully");
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
