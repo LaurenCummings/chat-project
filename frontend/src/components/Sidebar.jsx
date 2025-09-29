5dd14c6668ed82bae99623206cae87b55d6121ac
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-29 01:30:54 -0400

    imported state constants from chatStore into sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,7 @@
+import { useChatStore } from "../store/useChatStore";
+
 const Sidebar = () => {
+  const { getUsers, users, selectedUser, setSelectedUser } = useChatStore();
   return <div>Sidebar</div>;
 };
 
