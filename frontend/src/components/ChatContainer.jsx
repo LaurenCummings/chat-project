c70848210b5f323c179e4d86c89a688939b62292
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-09-29 15:45:38 -0400

    imported state constants from chatStore into chatContainer

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,8 @@
+import { useChatStore } from "../store/useChatStore";
+
 const ChatContainer = () => {
+  const { messages, getMessages, isMessagesLoading } = useChatStore();
+
   return <div>ChatContainer</div>;
 };
 
