017a219a49a92529273ea43a593e5b5b975692ff
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-01 23:34:53 -0400

    mapped messages and displayed appropriate avatars for each message

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,10 +3,13 @@ import { useEffect } from "react";
 import MessageSkeleton from "../components/skeletons/MessageSkeleton";
 import ChatHeader from "../components/ChatHeader";
 import MessageInput from "../components/MessageInput";
+import { useAuthStore } from "../store/useAuthStore";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } =
     useChatStore();
+
+    const { authUser } = useAuthStore();
 
   useEffect(() => {
     getMessages(selectedUser._id);
@@ -23,7 +26,20 @@ const ChatContainer = () => {
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
-      <p>messages...</p>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        { messages.map((message) => (
+          <div 
+            key={message._id}
+            className={`chat ${message.sender._id === authUser._id ? "chat-end" : "chat-start"}`}
+          >
+            <div className="chat-image avatar">
+              <div className="size-10 rounded-full border">
+                <img src={message.senderId === authUser._id ? authUser.profilePic || "/avatar.png" : selectedUser.profilePic || "/avatar.png"} alt="profile pic" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
       <MessageInput />
 
     </div>
