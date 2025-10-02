d9647c3a5b53ff54faaf6edb3c2cae1edad4c185
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-01 23:47:28 -0400

    added formatting to message times

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -4,6 +4,7 @@ import MessageSkeleton from "../components/skeletons/MessageSkeleton";
 import ChatHeader from "../components/ChatHeader";
 import MessageInput from "../components/MessageInput";
 import { useAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
   const { messages, getMessages, isMessagesLoading, selectedUser } =
@@ -39,7 +40,7 @@ const ChatContainer = () => {
             </div>
             <div className="chat-header mb-1">
               <time className="text-xs opacity-50 ml-1">
-                {message.createdAt}
+                {formatMessageTime(message.createdAt)}
               </time>
             </div>
             <div className="chat-bubble flex">
