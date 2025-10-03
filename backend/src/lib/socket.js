bc5c68806f44e180c129f5612b95a26c98b68134
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-03 00:26:21 -0400

    added emit function to let all users know when a new user is online

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -20,6 +20,9 @@ io.on("connection", (socket) => {
     const userId = socket.handshake.query.userId;
     if (userId) userSocketMap[userId] = socket.id;
 
+    // io.emit() is used to send events to all the connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
     socket.on("disconnect", () => {
         console.log("A user disconnected", socket.id);
     })
