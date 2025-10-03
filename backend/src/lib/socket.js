77e892d5f3c6b05a5395f26626389d64fa3e832b
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-03 00:27:38 -0400

    deleted user from socket map when they disconnect and added emit function to let other users know that user is offline

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -25,6 +25,8 @@ io.on("connection", (socket) => {
 
     socket.on("disconnect", () => {
         console.log("A user disconnected", socket.id);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
     })
 })
 
