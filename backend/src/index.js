999387ec9c43eb66566bb2cdd47829840803ab29
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-02 23:23:41 -0400

    deleted express app from index file and imported the one from socket file instead

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,10 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
