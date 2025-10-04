e6e2112edf6787bbb6931aee97d12c2f467056bb
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-04 00:27:19 -0400

    grabbed directory name using path

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,12 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
