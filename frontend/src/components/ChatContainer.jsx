c8e8bd5c24efd96e69202226211cf737d08295ea
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-10-03 22:42:44 -0400

    added useEffect to scroll to bottom of messages when message array changes

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -21,6 +21,12 @@ const ChatContainer = () => {
     return () => unsubscribeFromMessages();
   }, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
 
+  useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: smooth });      
+    }
+  },[messages]);
+
   if (isMessagesLoading) return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
@@ -37,6 +43,7 @@ const ChatContainer = () => {
           <div 
             key={message._id}
             className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}
+            ref={messageEndRef}
           >
             <div className="chat-image avatar">
               <div className="size-10 rounded-full border">
